6463ec5ff249e1e8776d2aa3b2012c3829b806af
Ami0480 <amifuku80@gmail.com>
2025-08-27 13:49:54 +0800

    Linked footer email to thankyou.html

--- src/index.js
+++ src/index.js
@@ -4,3 +4,18 @@ const newPage = document.getElementById("new-page");
 
 openBtn.addEventListener("click", () => newPage.classList.add("active"));
 closeBtn.addEventListener("click", () => newPage.classList.remove("active"));
+
+function displayThankYou(event) {
+  event.preventDefault();
+
+  let inputEmail = document.querySelector("#email-input").value;
+
+  if (inputEmail) {
+    window.location.href = "thankyou.html";
+  } else {
+    alert("Please enter your email");
+  }
+}
+
+let arrowButton = document.querySelector("#arrow");
+arrowButton.addEventListener("click", displayThankYou);
