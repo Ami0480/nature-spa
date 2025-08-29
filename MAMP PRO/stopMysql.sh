f568661ec4883fa91cc29cccda3aa276d73a6fc9
Ami0480 <amifuku80@gmail.com>
2025-08-29 14:15:24 +0800

    Created booking form

--- MAMP PRO/stopMysql.sh
+++
@@ -1,2 +0,0 @@
-#!/bin/sh
-/Applications/MAMP/Library/bin/mysql80/bin/mysqladmin --defaults-file=/Applications/MAMP/tmp/mysql/my.cnf --user=root --socket=/Applications/MAMP/tmp/mysql/mysql.sock shutdown
